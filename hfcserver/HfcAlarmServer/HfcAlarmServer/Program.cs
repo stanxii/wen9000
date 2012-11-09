99eb95a3cf8f61d20b93a02eb406103f365860c5
nixinlong <may2250@163.com>
2012-11-09 13:09:27 +0800

    add hfc alarmserver

---
+++ hfcserver/HfcAlarmServer/HfcAlarmServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+using System.Text;
+using adventnet.snmp.mibs;
+using adventnet.snmp.snmp2; 
+
+namespace HfcAlarmServer
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+
+            CAppKernel app = new CAppKernel();
+            
+        }
+
+
+        
+
+    }
+}
