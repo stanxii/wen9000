7c916cd2f154ba7de22aefd93f9a58b25fcf629c
Stan HangZhou <stanhangzhou@gmail.com>
2013-08-28 09:59:49 +0800

    version v1.1.22 2013-8-28

--- node.js/public/javascripts/doversion.js
+++ node.js/public/javascripts/doversion.js
@@ -24,7 +24,7 @@
 				   '<table>'+
 				    '<tr><td><label>版本hash值：</label></td><td>'+
 				    
-				    '<label>26f79a31482083ba32ee1ab13b8c8b717edaa271</label>'+
+				    '<label>50720aa235825dc5ad87b2d0cd71e0f50a2d6655</label>'+
 				    
 				   '</td></tr>'+
 				   '<tr><td><label>版本号:：</label></td><td>'+
